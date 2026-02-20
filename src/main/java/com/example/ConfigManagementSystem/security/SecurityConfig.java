aa52805b48e7e07d28fb8d0446843814119357d6
pranav-nani <pranavreddy1412@gmail.com>
2026-02-20 21:37:33 +0530

    Frontend Url updated

--- src/main/java/com/example/ConfigManagementSystem/security/SecurityConfig.java
+++ src/main/java/com/example/ConfigManagementSystem/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://config-managment-git-main-pranavnanis-projects.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://config-managment.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
         configuration.setAllowCredentials(true);
